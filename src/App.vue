e27622e66d860d2fa30fd371efb5a5f0e2068393
GorilaNaranja <woodworkerdeveloper@gmail.com>
2020-11-21 17:20:30 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Index />
+  </div>
+</template>
+
+<script>
+import Index from "./pages/index.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: { Index },
+  created() {},
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const state = {
+  bills: [],
+};
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+const store = new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations,
+});
+
+export default store;
